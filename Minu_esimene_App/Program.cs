a7c55c159a8b0ed8fb717eeb1b57f4485bf78f5e
Boriss-Errapart <ootasa@msn.com>
2022-09-27 23:47:05 +0300

    loon uut appi

---
+++ Minu_esimene_App/Program.cs
@@ -0,0 +1,14 @@
+﻿// Antud projekt on loodud Boriss Errapart poolt, rühmast KK2020
+// Rakendus küsib kasutajalt tema nime.
+// Rakendus tervitab kasutajat nimepidi.
+
+
+Console.WriteLine("Sisesta oma nimi:");
+// string - sõne
+string userName;
+// kuidas ma loen kasutaja nime. Eelmise ja järgmise rea saab ka kokku tõmmata ühele reale.
+userName = Console.ReadLine();
+// Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Tere" + ", " + userName + "!");
+// String interpolation (katse 2) dollari märk annab teada, et kasutad muutujat, dollari märk on: alt(parem) + 4, erimärgid: alt + 7.
+Console.WriteLine($"Tere, {userName} Errapart!");
